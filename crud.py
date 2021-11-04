fc277649ab83ad8e97983b4d27b9847a28c1f331
Molly Riley <mo.riley@gmail.com>
2021-11-04 17:25:18 +0000

    making api form

--- crud.py
+++ crud.py
@@ -43,33 +43,38 @@ def get_users():
     #returns all users
     return User.query.all()
 
+
 def get_user_by_id(user_id):
     #returns one user
     return User.query.get(user_id)
+
 
 def get_user_by_email(email):
     #get user from email to see if in database already
     #.first() brings up just object, not list of object.
     return User.query.filter(User.email == email).first()
 
+
 #Search functions
 def get_searches():
     #returns all searches
     return Search.query.all()
 
+
 #def get_search_by_id(search_id):
     #returns search object by id
  #   return Search.query.filter(Search.search_id == search_id).first()
+
 
 def get_search_by_id(search_id):
     return Search.query.get(search_id)
     #returns one search
 
+
 def get_searches_for_user(user_id):
-    searches_by_user = Search.query.filter(Search.user_id ==user_id).all()
+    searches_by_user = Search.query.filter(Search.user_id==user_id).all()
     return searches_by_user
     #returns a list of search objects by user
-
 
 
 def most_recent_search():
@@ -83,3 +88,4 @@ def most_recent_search():
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
+    #connects to Flask app, runs page on localhost
--- server.py
+++ server.py
@@ -18,66 +18,88 @@ def homepage():
     """View homepage."""
     return render_template('homepage.html')
 
+
+## User and Login Routes ##
+
 @app.route('/users')
 def all_users():
     """view all users list"""
     users = crud.get_users()
     return render_template("allusers.html", users=users)
+    #may be able to delete this route when all user page deleted. 
+
 
 @app.route("/users/<user_id>")
 def show_user(user_id):
     """ Show user's profile page. """
     user = crud.get_user_by_id(user_id)
-    return render_template("user.profile.html", user=user)
+    my_searches = crud.get_searches_for_user(user_id)
+    return render_template("user.profile.html", user=user, my_searches=my_searches)
+    #returns user object and list of searches by user to user profile
+    #may be able to delete ths route when all user page deleted.
+
 
 @app.route("/users", methods=["POST"])
 def register_user():
+    #Creating a user account
+    # It gets the email and password the user put in, and regiters user in database
     email = request.form.get("email")
     password = request.form.get("password")
 
+    #if email already in users database, flash message and redirect
     user = crud.get_user_by_email(email)
     if user:
-        flash("You are already registred, please try another email")
+        flash("You are already registred, please try another email or log in")
 
     else:
+        #creeate user with the email and password user typed
         crud.create_user(email, password)
         flash("Account has been created, please login")
 
-    return redirect("/")
-#change to login page once set up
+    #redirects to login/create account page   
+    return redirect("/login.page")
+
 
 @app.route("/login", methods = ["POST"])
 def login_page(): 
-
+    #Login process for user
+    #email, password are from user input in form
+    #user is pulled up by email
     email = request.form.get("email")
     password = request.form.get("password")
     user = crud.get_user_by_email(email)
 
+    #if user doesn't exist in database or password is wrong: 
+    #flash message and send to homepage
     if not user or user.password != password:
-        flash("Incorrect password")
+        flash("Incorrect password, or need to register")
         return redirect("/")
     else:
+        #add user email to session, get searches by user to show on user profile, if any
+        #send flash message, and log user in to their user profile
         session["user_email"] = user.email
+        session["user"] = user
+        my_searches = crud.get_searches_for_user(user.user_id)
         flash("Logged In!")
-        return render_template('user.profile.html', user=user)
-        #same problem as line 71
+        return render_template('user.profile.html', user=user, my_searches=my_searches)
+
 
 @app.route('/<user_id>')
 def show_profile(user_id):
     """show a user's profile with their searches"""
+    #loop searches on html page.  Section doesn't display if my_searches is empty.
     user = crud.get_user_by_id(user_id)
-    my_searches = crud.get_searches_for_user(user)
+    my_searches = crud.get_searches_for_user(user_id)
     return render_template('user.profile.html', user=user, my_searches=my_searches)
-    ##what if no searches yet? 
 
-    # @app.route("/tbd")
-# def show_user_searches(user_id):
-#     user = crud.get_user_by_id(user_id)
-# #####
 
 @app.route('/login.page')
 def go_to_login():
+    #Send user to login page
     return render_template('login.page.html')
+
+
+## Search Routes ##
 
 @app.route('/searches')
 def all_searches():
@@ -85,11 +107,13 @@ def all_searches():
     searches = crud.get_searches()
     return render_template("allsearches.html", searches=searches)
 
+
 @app.route("/searches/<search_id>")
 def show_search(search_id):
     """ Show a zipcode search page. """
     search = crud.get_search_by_id(search_id)
     return render_template("search.details.html", search=search)
+
 
 @app.route("/searches/extended/<search_id>")
 def show_extendeed_search(search_id):
@@ -98,7 +122,36 @@ def show_extendeed_search(search_id):
     return render_template("search.extended.html", search=search)
 
 
+## API Routes ##
+
+
+@app.route("/newsearch", methods = ["POST"])
+def new_user_search():
+    """Create a new API search with user input"""
+    #get zipcode
+    #get date
+    #get 7 day option - later
+    zipcode = request.form.get("zicode")
+    date = request.form.get("date")
+        #or datetime.now()
+    user = session[user]
+
+    #If API doesn't work, not enough info: 
+    if not API: 
+        flash("Sorry, that zipcode doesn't have enough air quality data for this search.  Enter another zipcode.")
+        redirect("/user.profile/<user_id>")
+
+    if API: 
+        flash("Click on the new search below to see details")
+
+        #ozone, pm, category come from API
+
+        crud.create_search(user, date, zipcode, ozone, pm, category)
+
+
+#Footer
 if __name__ == "__main__":
     #DebugToolbarExtension(app)
     connect_to_db(app)
+    #Connects to Flask, on localhost
     app.run(host="0.0.0.0", debug=True)
