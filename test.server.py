c260734793bdba19a1b769bc70846c6d3adf4d30
Molly Riley <mo.riley@gmail.com>
2021-11-28 00:10:54 +0000

    5 working tests and 1 not working

--- test.server.py
+++ test.server.py
@@ -6,18 +6,18 @@ import unittest, doctest
 from unittest import TestCase
 from server import app
 from model import connect_to_db, db, example_data
-from flask import session
+from flask import session, jsonify
 
 
 def load_tests(loader, tests, ignore):
-    """run the tests in other places.
+    """Runs the tests in other places.
     There is a doctest in server.py.  Working."""
     tests.addTests(doctest.DocTestSuite(server))
     return tests
 
 
 class FlaskTestsDatabase(TestCase):
-    """Flask tests that use the database."""
+    """Flask tests that use the database.  All air quality prioject pages use the database."""
 
     def setUp(self):
         """Before every test."""
@@ -47,7 +47,7 @@ class FlaskTestsDatabase(TestCase):
         self.assertIn(b'<h1 id="doctest">Welcome to Air Quality Project Page</h1>', result.data)
 
     def test_login(self):
-        """Test login page. Working."""
+        """Test login route. Working."""
 
         result = self.client.post("/login",
                                   data={"email": "1@mail.com", "password": "123"},
@@ -64,11 +64,23 @@ class FlaskTestsDatabase(TestCase):
     def test_savesearch_route(self):
         """Test that search is saved with api result data."""
         result = self.client.post("/savesearch",
-                                  data={'''[{DateObserved: "2021-11-22 ", HourObserved: 0, LocalTimeZone: "CST", 
-                                  ReportingArea: "Minneapolis-St. Paul", StateCode: "MN", Latitude: 44.955,
-                                  Longitude: -93.185, ParameterName: "PM2.5", AQI: 19, Category: {Number: 1, Name: "Good"}}]'''},
+                                  data={[{"DateObserved": "2021-11-22 ", "HourObserved": 0, "LocalTimeZone": "CST",
+                                  "ReportingArea": "Minneapolis-St. Paul", "StateCode": "MN", "Latitude": 44.955,
+                                  "Longitude": -93.185, "ParameterName": "PM2.5", "AQI": 19, "Category": {"Number": 1, "Name": "Good"}}]},
                                   follow_redirects=True)
+                                  #This is normally a list with the dictionary as iitem 0.  The placement of quotes didn't work for
+                                  #it to run as normal, so its format is edited a little vs. what is really coming from the API.
+                                  #not able to get this as a string, only unhashable dict, list.
+        print(result.data)
         self.assertIn(b"search saved", result.data)
+
+
+    def test_user_profile_page(self):
+        """Test that a user profile shows one of their searches. Working."""
+
+        result = self.client.get("/users/2")
+        #print(result.data)
+        self.assertIn(b"Search #2", result.data)
 
 
 ## note for saving database before testing
